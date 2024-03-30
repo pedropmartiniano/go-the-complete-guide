a208a1441866b66f0266394678640594a245938c
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2024-03-30 17:24:15 -0300

    updates

--- functions/main.go
+++ functions/main.go
@@ -3,15 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	num := factorial(5)
+	numbers := []int{1, 2, 3, 4, 5}
 
-	fmt.Println(num)
+	// spread operator
+	// desestrurando o array e passando cada indice como um parametro
+	sum := sumup(0, numbers...)
+	sum2 := sumup(0, 1, 2, 3, 4, 5, 6, 7)
+
+	fmt.Println(sum)
+	fmt.Println(sum2)
 }
 
-func factorial(num int) int {
-	if num == 1 {
-		return 1
+// função que recebe N parametros
+// irá colocar todos os parametro dentro de uma slice(array) de inteiro
+func sumup(startingValue int, numbers ...int) int {
+	for _, val := range numbers {
+		startingValue += val
 	}
 
-	return num * factorial(num-1)
+	return startingValue
 }
---
+++ functions/recursion/main.go
@@ -0,0 +1,17 @@
+package recursion
+
+import "fmt"
+
+func main() {
+	num := factorial(5)
+
+	fmt.Println(num)
+}
+
+func factorial(num int) int {
+	if num == 1 {
+		return 1
+	}
+
+	return num * factorial(num-1)
+}
